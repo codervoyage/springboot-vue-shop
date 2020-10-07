8e86c04893c9ac32787c9be5857ec208c71770a7
codervoyage <rainbow_email@126.com>
2020-10-07 16:44:33 +0800

    配置vue框架，梳理结构

--- vue-shop/src/main.js
+++ vue-shop/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Axios from 'axios'
 createApp(App).use(router).mount('#app')
+
+Axios.defaults.baseURL = 'http://localhost:8080/'
+
+Vue.prototype.$http = Axios
+
