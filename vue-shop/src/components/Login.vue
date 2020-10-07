34eb6da308cd5c47bd6832be9e42471cd98e677a
codervoyage <rainbow_email@126.com>
2020-10-07 20:25:45 +0800

    构架vueue

---
+++ vue-shop/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div id="boxContainer">
+    <div id="box">
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+#boxContainer {
+  background: rgb(45, 58, 75);
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#box {
+  width: 500px;
+  height: 350px;
+  border: 1px solid black;
+}
+</style>
---
+++ vue-shop/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element-ui'
+import './assets/css/global.css'
+import Axios from 'axios'
+Axios.defaults.baseURL = 'http://localhost:8081/'
+Vue.prototype.$http = Axios
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
