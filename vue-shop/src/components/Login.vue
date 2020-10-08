9c32df5294cdebc9f908d887ffdd7ed088b7ce57
liu221171 <liu150426221171@163.com>
2020-10-08 11:07:47 +0800

    home

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -2,7 +2,24 @@
 
   <div id="boxContainer">
     <div id="box">
-      111111
+      <h3 id="ph">请登录</h3>
+
+       <el-form id="loginForm" :rules="loginRules" ref="loginFormRef"  :model="loginFrom">
+         <div id="box1">
+             <el-form-item class="efi" label="账号" prop="username">
+               <el-input class="loginInput" v-model="loginFrom.username"></el-input>
+             </el-form-item>
+
+           <el-form-item class="efi" label="密码" prop="password">
+             <el-input class="loginInput" type="password" v-model="loginFrom.password"></el-input>
+           </el-form-item>
+         </div>
+           <el-form-item id="box2">
+               <el-button type="primary" @click="submitForm()">登录</el-button>
+               <el-button>重置</el-button>
+           </el-form-item>
+       </el-form>
+
     </div>
   </div>
 
@@ -10,7 +27,41 @@
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+
+  data(){
+    return{
+      loginFrom:{
+        username:'admin',
+        password:'admin123'
+      },
+      loginRules:{
+        username:[
+          { required: true, message: '请输入username', trigger: 'blur' }
+        ],
+        password:[
+          { required: true, message: '请输入password', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods:{
+    submitForm(){
+      this.$refs.loginFormRef.validate(async valid =>{
+        console.log(valid);
+        if(!valid) return
+        const {data} = await this.$http.post('login',this.loginFrom)
+        if (data.meta.status === 200) {
+          this.$message.success("登录成功");
+          this.$router.push("/home");
+        } else {
+          this.$message.error(data.meta.msg);
+        }
+
+      })
+
+    }
+  }
 }
 </script>
 
@@ -23,8 +74,35 @@ export default {
   align-items: center;
 }
 #box {
-  width: 500px;
+  width: 400px;
   height: 350px;
-  border: 1px solid black;
+/*  border: 1px solid black;*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: wrap;
 }
+#box1{
+  width: 400px;
+}
+#loginForm{
+  display: flex;
+  justify-content: center;
+  flex-flow: wrap;
+
+}
+#box2{
+display: flex;
+  justify-content: flex-end;
+  width: 380px;
+}
+#ph{
+  color: aliceblue;
+  width: 200px;
+  text-align: center;
+}
+.loginInput{
+  width: 340px;
+}
+
 </style>
--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 const Login = () => import('../components/Login')
+const Home = () => import('../components/Home')
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/' , redirect:'/login' },
-  { path:'/login',component:Login}
+  { path:'/login',component:Login},
+  { path:'/home',component:Home}
 ]
 
 const router = new VueRouter({
